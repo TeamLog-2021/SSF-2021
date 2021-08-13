d3049e31ff6e93eb781288eb2c1d0070a478d4ab
2nat <llyun_k@naver.com>
2021-08-14 05:38:28 +0900

    2021 소프트웨어 나눔 축제 TeamLog

---
+++ 2021 SSF/index.js
@@ -0,0 +1,13 @@
+function add_todo() {
+    // HTML 요소를 가져와 주세요 :D
+
+    // HTML 요소에 속성을 추가해 주세요 :D
+
+    // 삭제 버튼을 클릭했을 시 사용되는 이벤트
+    rm_btn.onclick = function() {
+        // 삭제 버튼을 클릭했을 때 어떤 동작을 할지 추가해 주세요 :D
+        let ls = this.parentElement.parentElement // 리스트 li 태그를 불러온 변수입니다 :D
+    }
+
+    // 자식 요소를 추가해 주세요 :D
+}
